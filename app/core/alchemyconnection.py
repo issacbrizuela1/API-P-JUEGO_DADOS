7c0e8234b0b952bc2f54f591ec5e9c5ae0ac8a5d
Issac <74998947+issacbrizuela1@users.noreply.github.com>
2023-06-18 19:10:28 -0600

    WIP:#:alchemy connection 1- se creo la clase de alchemyconnection para aprender a usar este paquete

--- app/core/alchemyconnection.py
+++ app/core/alchemyconnection.py
@@ -1,5 +1,6 @@
-from _typeshed import Incomplete
-from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
 
 class ALCHCONN(SQLAlchemy):
     
@@ -25,3 +26,49 @@ class ALCHCONN(SQLAlchemy):
     def delete(self):
         db.session.delete(self)
         db.session.commit()
+
+
+
+
+# Configurar la conexión a la base de datos
+engine = create_engine('mysql://root:@servidor/api_pruebas', echo=True)
+
+# Crear una sesión
+Session = sessionmaker(bind=engine)
+session = Session()
+
+# Definir una clase que mapee a una tabla en la base de datos
+Base = declarative_base()
+
+class Producto(Base):
+    __tablename__ = 'productos'
+
+    id = Column(Integer, primary_key=True)
+    nombre = Column(String(50))
+    precio = Column(Integer)
+
+    def __repr__(self):
+        return f"<Producto(nombre='{self.nombre}', precio={self.precio})>"
+
+# Crear la tabla en la base de datos
+Base.metadata.create_all(engine)
+
+# Crear un nuevo producto
+nuevo_producto = Producto(nombre='Producto 1', precio=100)
+session.add(nuevo_producto)
+session.commit()
+
+# Leer todos los productos
+productos = session.query(Producto).all()
+for producto in productos:
+    print(producto)
+
+# Actualizar un producto
+producto_a_actualizar = session.query(Producto).filter_by(nombre='Producto 1').first()
+producto_a_actualizar.precio = 150
+session.commit()
+
+# Eliminar un producto
+producto_a_eliminar = session.query(Producto).filter_by(nombre='Producto 1').first()
+session.delete(producto_a_eliminar)
+session.commit()
